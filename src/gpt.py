67b632fdda875560cc2fac928f52c0e718cbb3e8
Radovan Netuka <radovan.netuka@email.cz>
2026-08-07 20:19:03 +0200

    Top K

--- src/gpt.py
+++ src/gpt.py
@@ -20,6 +20,10 @@ class GptModel(nn.Module):
     #   variably, but with a risk of producing nonsense output
     temperature: float | int
 
+    # top K parameter for token selection process
+    # if provided, select top K probability values and choose only among them
+    top_k: int | None
+
     def __init__(self):
         super().__init__()
         self.tok_emb = nn.Embedding(VOCABULARY_SIZE, EMBEDDING_DIMENSIONS)
@@ -31,6 +35,7 @@ class GptModel(nn.Module):
         self.final_norm = LayerNormalization()
         self.output_layer = nn.Linear(EMBEDDING_DIMENSIONS, VOCABULARY_SIZE, bias=False)
         self.temperature = 1
+        self.top_k = None
 
     def forward(self, in_idx: Tensor) -> Tensor:
         batch_size, seq_len = in_idx.shape
@@ -53,14 +58,29 @@ class GptModel(nn.Module):
         total_size_mb = total_size_bytes / (1024 * 1024)
         return f'{total_size_mb:.2f} MB'
 
-    def generate_text(self, context: Tensor, max_new_tokens: int, context_size: int = CONTEXT_LENGTH) -> Tensor:
+    def apply_top_k(self, logits: Tensor) -> Tensor:
+        if self.top_k is not None:
+            logits, _ = torch.topk(logits, k=self.top_k)
+            min_value = logits[:, -1]
+            logits = torch.where(
+                logits < min_value,
+                torch.tensor(float('-inf')).to(logits.device),
+                logits
+            )
+        return logits
+
+    def apply_temperature(self, logits: Tensor) -> Tensor:
+        return logits / self.temperature
+
+    def generate_text(self, context: Tensor, max_new_tokens: int) -> Tensor:
         for _ in range(max_new_tokens):
-            cropped_context = context[:, -context_size:]
+            cropped_context = context[:, -CONTEXT_LENGTH:]
             with torch.no_grad():
                 logits = self(cropped_context)
 
             logits = logits[:, -1, :]          # last vector, corresponding to the next token
-            logits = logits / self.temperature  # scale with temperature
+            logits = self.apply_top_k(logits)
+            logits = self.apply_temperature(logits)  # scale with temperature
             probabilities = torch.softmax(logits, dim=-1)  # converts vector into probability distribution
 
             # select a token with large probability score (choose among largest values)
